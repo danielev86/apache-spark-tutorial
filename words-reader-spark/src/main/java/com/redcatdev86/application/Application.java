f020cbddee5af2251200f9688a0531f6eca724b3
danieleverrienti86 <danieleverrienti1986@gmail.com>
2023-11-19 16:51:19 +0100

    Second READER App: read words.txt file

---
+++ words-reader-spark/src/main/java/com/redcatdev86/application/Application.java
@@ -0,0 +1,45 @@
+package com.redcatdev86.application;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+
+    private static final String RESOURCE_PATH = "src/main/resources/";
+
+    public static void main(String[] args) {
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        if (System.getProperty("os.name").contains("Windows")){
+            String hadoopPath = System.getProperty("user.dir") + "/" + RESOURCE_PATH + "winutils-extra/hadoop/bin";
+            System.setProperty("hadoop.home.dir", hadoopPath);
+        }
+
+        SparkConf sparkConf = new SparkConf().setAppName("wordReader").setMaster("local[*]");
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> originalTxt = jsc.textFile(RESOURCE_PATH+ "input/words.txt");
+        JavaRDD<String> words = originalTxt.flatMap(rawLine ->  splitData(rawLine).iterator());
+        words.foreach(word -> System.out.println(word));
+
+        long element = words.map(data -> 1L).reduce((val1,val2) -> val1 + val2);
+        System.out.println("Num of words: " + element);
+
+    }
+
+    private static List<String> splitData(String data){
+        String rawLine = data.replace("," , "")
+                .replace(".", "")
+                .replace(";", "")
+                .replace(";", "");
+        return Arrays.asList(rawLine.split(" "));
+    }
+
+}
